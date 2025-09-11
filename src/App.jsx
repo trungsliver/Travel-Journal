5929b812acb1d18e098d7c0bab8a2b6073106d02
trungsliver <buiductrung.cva@gmail.com>
2025-09-11 16:42:12 +0700

    entry component template

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Entry from './components/Entry'
 
 export default function App() {
   return (
     <>
       <Header />
+      <Entry />
     </>
   )
 }
