f9c4dc826bd95b72b5d510bcadf8b01dea51d5c2
trungsliver <buiductrung.cva@gmail.com>
2025-09-11 15:25:38 +0700

    header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="https://cdn.dribbble.com/userupload/14137116/file/original-48fe5fefd8e89499ad9ae38162376dc9.jpg?format=webp&resize=400x300&vertical=center" alt="" className="nav-logo" />
+                <p className="nav-title">My Travel Journal</p>
+            </nav>
+        </header>
+    )
+}
