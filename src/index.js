b1ece97ceae43060bcc567b0038d67ddf7933cbd
trungsliver <buiductrung.cva@gmail.com>
2025-09-12 11:05:50 +0700

    review array.map()

---
+++ src/index.js
@@ -0,0 +1,40 @@
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+const numsSquare = nums.map(num => (num**2))
+console.log(numsSquare)
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+function capitalizeFirst(str) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+const capitalNames = names.map(name => capitalizeFirst(name));
+console.log(capitalNames);
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+const pokemonPtag = pokemon.map(poke => `<p>${poke}</p>`)
+console.log(pokemonPtag);
